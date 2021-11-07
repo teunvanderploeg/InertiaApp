38994b540bcddf19a9d20d57d1184c619c2a8412
teunvanderploeg <29253@ma-web.nl>
2021-11-07 12:27:41 +0100

    add css classes

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <div>
+            <div class="flex row">
                 <h1 class="font-bold text-lg">My App</h1>
-                <p class="text-sm ml-4">Welcome Back, {{ username }}</p>
+                <p class="text-sm ml-4 my-auto">Welcome Back, {{ username }}</p>
             </div>
             <Nav/>
         </header>
