a57b82f4c0355759bf5bd28d69b84fc261d4ff0e
teunvanderploeg <29253@ma-web.nl>
2021-11-10 19:49:08 +0100

    Use inertia form

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -8,24 +8,24 @@
                 Name
             </label>
             <input v-model="form.name" type="text" class="border border-gray-400 p-2 w-full" name="name" id="name" required>
-            <div v-if="$page.props.errors.name" v-text="$page.props.errors.name" class="text-red-500 text-xs mt-1 mb-4" />
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1 mb-4" />
 
             <label for="email" class="block md-2 uppercase font-bold text-xs text-gray700">
                 Email
             </label>
             <input v-model="form.email" type="email" class="border border-gray-400 p-2 w-full" name="email" id="email" required>
-            <div v-if="$page.props.errors.email" v-text="$page.props.errors.email" class="text-red-500 text-xs mt-1 mb-4" />
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1 mb-4" />
 
             <label for="password" class="block md-2 uppercase font-bold text-xs text-gray700">
                 Password
             </label>
             <input v-model="form.password" type="password" class="border border-gray-400 p-2 w-full" name="password" id="password" required>
-            <div v-if="$page.props.errors.password" v-text="$page.props.errors.password" class="text-red-500 text-xs mt-1 mb-4" />
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1 mb-4" />
 
         </div>
 
         <div class="md-6">
-            <button class="bg-green-600 text-white rounded py-2 px-4 hover:bg-green-700">
+            <button class="bg-green-600 text-white rounded py-2 px-4 hover:bg-green-700" :disabled="form.processing">
                 Submit
             </button>
         </div>
@@ -33,17 +33,17 @@
 </template>
 
 <script setup>
-import {reactive} from "vue";
-import {Inertia} from "@inertiajs/inertia";
+import {useForm} from '@inertiajs/inertia-vue3';
 
-let form = reactive({
+let form = useForm({
     name: '',
     email: '',
     password: '',
 });
 
+
 let submit = () => {
-    Inertia.post('/users', form);
+    form.post('/users');
 }
 </script>
 
